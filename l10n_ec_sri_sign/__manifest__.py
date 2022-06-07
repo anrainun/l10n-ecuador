6f271cc35e633ec092ad1e6d0ddc59c88c673170
alonso <enriquenun95@gmail.com>
2022-06-07 07:15:39 -0500

    modulo firma electronica - permite registrar firma sri - validar firma - firmar el xml

---
+++ l10n_ec_sri_sign/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 Alonso Nuñez
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "SRI SIGN",
+    "version": "15.0.1.0.0",
+    "author": "Alonso Nuñez",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": [
+        'security/ir.model.access.csv',
+        'views/sign_electronic.xml',
+        'views/menu.xml'
+    ],
+    "installable": True,
+}
---
+++ l10n_ec_sri_sign/models/choices.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 Alonso Nuñez
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+STATE_SIGN_ELECTRONIC = [
+    ("unverified", "Sin Verificar"),
+    ("valid", "Firma Valida"),
+    ("expired", "Firma Vencida"),
+]
+
+
